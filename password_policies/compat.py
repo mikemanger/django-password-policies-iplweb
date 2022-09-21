e0f90a90eb53272e1801ece4ea88193463e67d04
dvd.ria <d.ria@frankhood.it>
2022-09-21 14:49:47 +0200

    Fixed problem with user.is_authenticated() None

--- password_policies/compat.py
+++ password_policies/compat.py
@@ -1,6 +1,8 @@
 import django
 
 def is_authenticated(user):
-    if django.VERSION < (1, 10):
-        return user.is_authenticated()
-    return user.is_authenticated
+    if user:
+        if django.VERSION < (1, 10):
+            return user.is_authenticated()
+        return user.is_authenticated
+    return False
